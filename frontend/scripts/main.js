0f8214f5fc5b11b08b65276a342760be85573d84
Edson Glz <edzziton@hotmail.com>
2022-11-24 23:35:02 -0600

    Modificaciones al front para conectar BDD

---
+++ frontend/scripts/main.js
@@ -0,0 +1,37 @@
+function getTasks() {
+    axios.get('http://localhost:3000/tasks/?token=123').then((respuesta) => {
+        const tareas = respuesta.data;
+        const contenedor = document.getElementById('taskList');
+
+        tareas.forEach(element => {
+            const descripcion = element.descripcion;
+            const fila = `                
+                <li class="list-group-item border-0 d-flex align-items-center ps-0">
+                    <input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />
+                    ${descripcion}
+                </li>
+            `;
+            contenedor.innerHTML += fila;
+        });
+
+    })
+}
+
+function initCreateTask() {
+    const form = document.getElementById('formTask');
+    const campos = {};
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const elementosForm = this.querySelectorAll(".form-Item")
+        console.log(elementosForm);
+        elementosForm.forEach(function (elemento) {
+            campos[elemento.name] = elemento.value;
+        })
+        console.log(campos);
+
+        axios.post('http://localhost:3000/tasks/create?token=123', campos).then((respuesta) => {
+            console.log(respuesta);
+
+        });
+    })
+}
